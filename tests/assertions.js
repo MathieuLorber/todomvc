dcb52ddbd4f8efff4d058c8c7fbe84a9db155937
Mathieu Lorber <mathieu.lorber@gmail.com>
2013-11-10 13:57:18 +0100

    Storage tested (for Dart sample for the moment)

--- tests/assertions.js
+++ tests/assertions.js
@@ -1,40 +1,36 @@
 casper.assertDisplayedItemsCount = function(itemsNumber, message) {
-    this.test.assertEval(function (itemsAwaitedNumber) {
-        var items = document.querySelectorAll('#todo-list li');
-        var number = 0;
-        for(var i = 0 ; i < items.length ; i++) {
-            // this if to accept only displayed elements
-            // TODO see https://groups.google.com/forum/?fromgroups=#!topic/jquery-dev/4Ys5mzbQP08
-            // __utils__.visible seems not to work in this case...
-            if(items[i].offsetWidth > 0 || items[i].offsetHeight > 0) {
-                number++;
-            }
-        }
-        return number === itemsAwaitedNumber;
-    }, message, itemsNumber);
+	this.test.assertEval(function(itemsAwaitedNumber) {
+		var items = document.querySelectorAll('#todo-list li');
+		var number = 0;
+		for (var i = 0; i < items.length; i++) {
+			// this if to accept only displayed elements
+			// TODO see https://groups.google.com/forum/?fromgroups=#!topic/jquery-dev/4Ys5mzbQP08
+			// __utils__.visible seems not to work in this case...
+			if (items[i].offsetWidth > 0 || items[i].offsetHeight > 0) {
+				number++;
+			}
+		}
+		return number === itemsAwaitedNumber;
+	}, message, itemsNumber);
 };
 
 casper.assertLeftItemsString = function(leftItemsString, message) {
-    // Backbone for example does not update string since it's not displayed. It's a valid optimization
-    if(leftItemsString == '0 items left' && !this.visible('#todo-count')) {
-        this.test.assertTrue(true, 'Left items label is not displayed - ' + message);
-        return;
-    }
-    var displayedString = this.fetchText('#todo-count').replace(/\n/g, '').replace(/\s{2,}/g, ' ').trim();
-    this.test.assertEquals(displayedString, leftItemsString, message);
+	// Backbone for example does not update string since it's not displayed. It's a valid optimization
+	if (leftItemsString == '0 items left' && !this.visible('#todo-count')) {
+		this.test.assertTrue(true, 'Left items label is not displayed - ' + message);
+		return;
+	}
+	var displayedString = this.fetchText('#todo-count').replace(/\n/g, '').replace(/\s{2,}/g, ' ').trim();
+	this.test.assertEquals(displayedString, leftItemsString, message);
 };
 
 casper.assertStorage = function(storageSize) {
-    this.test.assertEval(function (storageSize) {
-    	return true;
-    	// TODO !
-    	/*
-        var storage = JSON.parse(window.localStorage.getItem('todos-vanilladart'));
-        if(storageSize === 0 && storage == null) {
-            return true;
-        }
-        var size = storage.length;
-        return size === storageSize;
-        */
-    }, 'Assert storage size is ' + storageSize, storageSize);
+	this.test.assertEval(function(storageSize, storageName) {
+		var storage = JSON.parse(window.localStorage.getItem(storageName));
+		if (storageSize === 0 && storage == null) {
+			return true;
+		}
+		var size = storage.length;
+		return size === storageSize;
+	}, 'Assert storage size is ' + storageSize, [storageSize, storageName()]);
 };
---
+++ tests/functions/dart.js
@@ -0,0 +1,3 @@
+var storageName = function() {
+	return 'todos-vanilladart';
+};
